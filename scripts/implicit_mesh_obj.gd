afe026129277afb84a3002fc59dae55507b948ee
Peter-Pyshnyy <62466617+Peter-Pyshnyy@users.noreply.github.com>
2025-05-03 20:14:11 +0200

    added the render_manager node

--- scripts/implicit_mesh_obj.gd
+++ scripts/implicit_mesh_obj.gd
@@ -2,11 +2,18 @@ class_name ImplicidSurface extends Node3D
 
 @export var voxel_grid: VoxelGrid
 @onready var control = $Control
+@onready var slider_pos_x: HSlider = $Control/VBoxContainer/HBPosX/VBoxContainer/slider_pos_x
+@onready var slider_pos_y: HSlider = $Control/VBoxContainer/HBPosY/VBoxContainer/slider_pos_y
+@onready var slider_pos_z: HSlider = $Control/VBoxContainer/HBPosZ/VBoxContainer/slider_pos_z
+
 
 func _ready():
-	$Control/VBoxContainer/HBPosX/VBoxContainer/slider_pos_x.value = position.x
-	$Control/VBoxContainer/HBPosY/VBoxContainer/slider_pos_y.value = position.y
-	$Control/VBoxContainer/HBPosZ/VBoxContainer/slider_pos_z.value = position.z
+	slider_pos_x.max_value = voxel_grid.resolution
+	slider_pos_y.max_value = voxel_grid.resolution
+	slider_pos_z.max_value = voxel_grid.resolution
+	slider_pos_x.value = position.x
+	slider_pos_y.value = position.y
+	slider_pos_z.value = position.z
 
 
 func hide_ui():
---
+++ scripts/render_manager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+@export var voxel_grid: VoxelGrid
+@export var surfaces: Array[ImplicidSurface]
+
+func master_function(value: int):
+	var result: int
+	
